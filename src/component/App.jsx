463999b8411d43f5a20397614c3f210a4cc889b3
restumardiko <mardikorestu@gmail.com>
2024-05-30 19:11:42 +0700

    first commit

---
+++ src/component/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="  h-8 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-red-500">
+          hallo
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
