66b196823e1256b8e7757a385f8a65cfd0ec8e2d
restumardiko <mardikorestu@gmail.com>
2024-08-02 10:00:02 +0700

    adding red on each section of drop down menu

--- src/component/navbar/subnav/sheetHov.jsx
+++ src/component/navbar/subnav/sheetHov.jsx
@@ -1,9 +1,9 @@
 function SheetHov({ list }) {
   console.log(list);
-  const ls = list.map((yea) => <li>{yea}</li>);
+  const ls = list.map((yea) => <li className="hover:bg-red-500">{yea}</li>);
   return (
-    <div className="absolute bg-white top-50 group-hover:block hidden -z-10 ">
-      <ul>{ls}</ul>
+    <div className="absolute bg-white top-50 group-hover:block hidden -z-10  ">
+      <ul className="">{ls}</ul>
     </div>
   );
 }
