372ca236386cb88ff5c01356973b9a993c744af6
restumardiko <mardikorestu@gmail.com>
2025-03-12 17:37:40 +0700

    add raw page on Account inside

--- src/pages/accountInside.jsx
+++ src/pages/accountInside.jsx
@@ -6,10 +6,62 @@ export default function AccountInside() {
       <Link to="/">
         <div id="titel">
           <h1>PERIPLUS</h1>
-          <h2>the largerst book store in indonesia</h2>
+          <h2>ONLINE</h2>
         </div>
       </Link>
-      <div id="form"></div>
+
+      <div id="login-border" className="w-auto h-auto">
+        <div id="signin_header">
+          <h1>Sign In to Your Account</h1>
+        </div>
+        <div id="table_form">
+          <table>
+            <tr>
+              <td>
+                <b>Email:</b>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  maxLength={20}
+                  placeholder="enter your email"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <b>Password:</b>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input type="password" name="password" id="" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  className="bg-blue-500 w-56"
+                  type="submit"
+                  value="login"
+                />
+              </td>
+            </tr>
+          </table>
+          <div id="link">
+            <h3 className="block" href="">
+              You don't have account yet ? Register now{" "}
+            </h3>
+            <h3 href="">Forgot your Password ?</h3>
+          </div>
+          <div>
+            <h3>Or Login With</h3>
+            <h1>GOOGLE</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
