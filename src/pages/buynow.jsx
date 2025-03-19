d004fcfa00d056430881280670fd5fb7eca4a472
restumardiko <mardikorestu@gmail.com>
2025-03-19 09:07:29 +0700

    update buynow pages

--- src/pages/buynow.jsx
+++ src/pages/buynow.jsx
@@ -1,9 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+
 import NavSub from "../component/navbar/navigation";
 import { BookContext } from "../component/App";
 
 export default function BuyNow() {
   const { buy } = useContext(BookContext);
+
+  const [quantity, setQuantity] = useState(1);
+
+  function plus() {
+    setQuantity(quantity + 1);
+  }
+
+  function minus() {
+    quantity !== 1 && setQuantity(quantity - 1);
+  }
+  function checkout() {}
   console.log(buy);
   return (
     <>
@@ -18,7 +30,31 @@ export default function BuyNow() {
             <h1>{buy.price}</h1>
           </div>
           <div id="button">
-            <div id="sum"></div>
+            <div id="how-much-item" className="">
+              <button
+                onClick={() => {
+                  minus(buy.id);
+                }}
+                id="decrease-item"
+              >
+                -
+              </button>
+              <h1>{quantity}</h1>
+              <button
+                onClick={() => {
+                  plus(buy.id);
+                }}
+                id="increase-item"
+              >
+                +
+              </button>
+            </div>
+            <div id="check_out">
+              <div id="total-amount" className="h-44 bg-gray-200 m-20 mt-3">
+                <h1>Total :Rp.{quantity * buy.price}</h1>
+                <button onClick={checkout}>CHECK OUT</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
