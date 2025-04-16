4d509f3ec926111d05e101d83217db0ce355e3a6
restumardiko <mardikorestu@gmail.com>
2025-04-16 17:14:08 +0700

    updated wishlist

--- src/pages/wishlistInside.jsx
+++ src/pages/wishlistInside.jsx
@@ -9,32 +9,47 @@ function WishlistBook() {
   //const[]
 
   return (
-    <ul>
+    <ul className="p-8 flex flex-col gap-2 ">
       {wish.map((book) => (
         <>
-          <li key={book.id} className="h-auto m-4">
-            <img className=" w-32 h-48" src={book.cover} alt="" />
-            <h1>{book.title}</h1>
-            <h1>{book.author}</h1>
-            <h1>Rp.{book["price"].toLocaleString()}</h1>
-            <h1>{book.year}</h1>
-            <h1>Shared this item ?</h1>
+          <li
+            key={book.id}
+            className="h-auto m-7  flex flex-row border-b-2 border-gray-300"
+          >
+            <img className="  h-56 inline mx-6" src={book.cover} alt="" />
+            <div className="inline-block w-full p-6">
+              <h1 className="text-2xl font-semibold my-2">{book.title}</h1>
+              <h2 className="text-xl font-semibold italic text-gray-500 my-1">
+                Paperback
+              </h2>
+              <h1 className="font-semibold text-blue-700 text-xl">
+                {book.author}
+              </h1>
+              <h1 className="text-red-600 font-semibold text-xl">
+                Rp. {book["price"].toLocaleString()}
+              </h1>
 
-            <button
-              onClick={() => {
-                handleEdite(book.id);
-              }}
-            >
-              Edit
-            </button>
+              <h1 className="text-2xl py-6">Shared this item ?</h1>
+              <div className="py-2">
+                <button
+                  className="text-2xl py-2 px-5"
+                  onClick={() => {
+                    handleEdite(book.id);
+                  }}
+                >
+                  EDIT
+                </button>
 
-            <button
-              onClick={() => {
-                handleDelete(book.id);
-              }}
-            >
-              Delete
-            </button>
+                <button
+                  className="bg-gray-700 px-5 py-2 font-semibold text-white text-2xl hover:bg-orange-400"
+                  onClick={() => {
+                    handleDelete(book.id);
+                  }}
+                >
+                  DELETE
+                </button>
+              </div>
+            </div>
           </li>
         </>
       ))}
@@ -47,24 +62,31 @@ function WishListInside() {
   return (
     <div className="pt-[9.5rem]">
       <NavSub title={"Wish List"} />
-      <div id="background" className="bg-white  h-auto block">
+      <div id="background" className="  h-auto w-[85%] mx-auto mt-10 ">
         <div
           id="title-container"
-          className="bg-white w-3/4 m-5 inline-block h-40"
+          className="bg-white w-full  inline-block h-auto shadow-md text-5xl font-bold text-gray-600 p-10 rounded"
         >
-          My Wish List
+          <h1>My Wish List</h1>
         </div>
-        <div id="input-container" className="w-3/4 m-5 inline-block h-auto p-5">
+        <div className="w-full my-5 bg-white inline-block h-auto p-10 text-[1.5rem] shadow-md rounded">
           <input
             type="form"
             maxLength={20}
-            className="m-2"
+            className=" h-12 border-gray-300 border-2  my-2 px-4 rounded "
             placeholder="Enter name or e-mail"
           ></input>
-          <button className="bg-blue-700">GO</button>
-          <h3>Find someone's Wish List</h3>
+          <button className="bg-blue-700 w-40 text-white h-12 mx-1 rounded ">
+            GO!
+          </button>
+          <h3 className="text-gray-500 font-semibold">
+            Find someone's Wish List
+          </h3>
         </div>
-        <div id="item-container" className="w-3/4 m-5 h-auto inline-block">
+        <div
+          id="item-container"
+          className="w-full bg-white shadow-md my-5 h-auto inline-block rounded"
+        >
           <WishlistBook />
         </div>
       </div>
