4b6805941fcfa6e7b7aa82c1d3e56148f4bbf506
restumardiko <mardikorestu@gmail.com>
2025-05-10 16:19:31 +0700

    update account inside

--- src/pages/accountInside.jsx
+++ src/pages/accountInside.jsx
@@ -8,7 +8,7 @@ export default function AccountInside() {
           {/* <h1>PERIPLUS</h1>
           <h2>ONLINE</h2> */}
           <img
-            className="w-40 m-auto py-4"
+            className="w-44 m-auto py-4"
             src="src/logo-periplus.jpg"
             alt=""
           />
@@ -17,19 +17,19 @@ export default function AccountInside() {
 
       <div
         id="login-border"
-        className="lg:w-[30%] mx-auto my-4 border-2 border-gray-300 p-7  h-auto"
+        className="lg:w-[30%] lg:mx-auto mt-4 border-[0.1rem] border-gray-400 p-3 mx-2  h-auto"
       >
         <div
           id="signin_header"
-          className="text-left font-semibold   text-xl text-orange-600 font-sans mb-8 "
+          className="text-left font-bold   text-xl  text-orange-600 font-sans mb-4 "
         >
           <h1>Sign In to Your Account</h1>
         </div>
         <div
           id="table_form"
-          className="w-full text-xl text-gray-800  rounded-lg"
+          className="w-full text-[1.1rem]  text-gray-800  rounded-lg"
         >
-          <table className="w-full my-2  ">
+          <table className="w-full my-1  ">
             <tr>
               <td>
                 <b>Email:</b>
@@ -41,7 +41,7 @@ export default function AccountInside() {
                   type="text"
                   maxLength={20}
                   placeholder=""
-                  className="border-solid border rounded-md h-12 w-full my-3 text-2xl "
+                  className="border-solid border rounded-md h-8 w-full  text-sm px-3 "
                 />
               </td>
             </tr>
@@ -56,14 +56,14 @@ export default function AccountInside() {
                   type="password"
                   name="password"
                   id=""
-                  className="border-solid border rounded-md h-12 w-full my-3 text-2xl "
+                  className="border-solid border rounded-md h-8 w-full text-xl px-3 "
                 />
               </td>
             </tr>
             <tr>
               <td>
                 <input
-                  className="bg-blue-600 w-full text-balance text-xl h-10  text-white my-3 border-solid border-2 border-black rounded-md"
+                  className="bg-blue-600 w-full text-balance text-base h-7  text-white my-1 border-solid border-[0.1rem]  border-black rounded-md"
                   type="submit"
                   value="Login"
                 />
@@ -71,15 +71,15 @@ export default function AccountInside() {
             </tr>
           </table>
         </div>
-        <div id="link" className="m-auto text-2xl text-blue-800 my-5">
+        <div id="link" className="m-auto text-xs text-blue-800 mt-4">
           <h3 className="block " href="">
             You don't have account yet ? Register now{" "}
           </h3>
           <h3 href="">Forgot your Password ?</h3>
         </div>
-        <div className="text-center text-xl">
+        <div className="text-center text-xs text-gray-600 mt-2">
           <b>Or Login With</b>
-          <img src="src/google.png" className="w-52 mx-auto my-5" />
+          <img src="src/google.png" className="w-28 mx-auto my-3" />
         </div>
       </div>
     </div>
